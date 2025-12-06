a1c925b6d378349e3dfc3f31fada8a19f09adf8e
harena-mihaja <mihaja2506@gmail.com>
2025-12-06 15:17:30 +0300

    feat: add schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Product (
+    id integer primary key,
+    name varchar(255) not null,
+    price numeric not null,
+    creation_datetime timestamp default current_timestamp not null
+);
+
+CREATE TABLE Product_category (
+    id integer primary key,
+    name varchar(255) not null,
+    product_id integer not null,
+    FOREIGN KEY (product_id) references product(id)
+);
