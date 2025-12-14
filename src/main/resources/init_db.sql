fd9df4df73b65bcb84694212a1c597601f9f1f60
harena-mihaja <mihaja2506@gmail.com>
2025-12-14 09:12:44 +0300

    refactor: redefine user permission on Database

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,2 +1,13 @@
+-- Create DB and user
+CREATE DATABASE product_management_db;
 CREATE USER product_manager_user WITH PASSWORD '123456';
-CREATE DATABASE product_management_db OWNER product_manager_user;
+
+-- Allow user to connect to DB
+GRANT CONNECT ON DATABASE product_management_db TO product_manager_user;
+
+-- Connect to DB
+\c product_management_db
+
+-- Allow user to CRUD in DB
+GRANT CREATE ON SCHEMA public TO product_manager_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO product_manager_user;
